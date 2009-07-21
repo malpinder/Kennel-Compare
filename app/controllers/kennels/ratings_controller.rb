9024d366333f71ba171ba119a162e1ff7120c25d
Sarah <sarah@Mauve.(none)>
2009-07-21 11:50:05 +0100

    Set up project to use factory_girl; refactored ratings_controller and tests

--- app/controllers/kennels/ratings_controller.rb
+++ app/controllers/kennels/ratings_controller.rb
@@ -7,14 +7,10 @@ class Kennels::RatingsController < ApplicationController
     @kennel = Kennel.find(params[:kennel_id])
     
     @kennel.number_of_ratings += 1
-    @kennel.rating = calculate_new_rating(@kennel, params[:rating][:rating])
+    new_rating = (@kennel.rating + params[:rating][:rating].to_i)/(@kennel.number_of_ratings)
+    @kennel.rating = new_rating
 
     @kennel.save
     redirect_to kennel_path(@kennel)
-  end
-
-  def calculate_new_rating(kennel, rating)
-    old_rating = kennel.rating
-    new_rating = (old_rating + rating.to_i)/(kennel.number_of_ratings)
   end
 end
